b0b4b67ab794f5a4994887f4aca31dfb9f47b94b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-11-06 15:56:24 +0300

    Recommend Movie Page / Added

---
+++ Asp.NetCore5.0_Movie_Project/ViewComponents/RecommendFilms.cs
@@ -0,0 +1,24 @@
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_Movie_Project.ViewComponents
+{
+    
+    public class RecommendFilms:ViewComponent
+    {
+        MovieManager movieManager = new MovieManager(new EfMovieRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = movieManager.GetMovieListWithCategory();
+            return View(values);
+        }
+
+    }
+}
